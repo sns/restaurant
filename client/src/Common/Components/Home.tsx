cf7d4bc7c0a982a36001ff196fcefea256692c7d
Sina Salehi <sinasalehi@Sinas-MacBook-Pro.local>
2019-04-17 23:34:20 -0400

    fixed the parallax

--- client/src/Common/Components/Home.tsx
+++ client/src/Common/Components/Home.tsx
@@ -2,20 +2,19 @@ import * as React from "react";
 
 const styles = {
   body: {
-    marginTop: 100,
+    marginTop: 100
   } as React.CSSProperties,
   parallax: {
     backgroundImage: `url('${process.env.PUBLIC_URL}/images/sofre1.jpg')`,
-    height: 500,
+    height: 600,
     width: "100%",
     backgroundAttachment: "fixed",
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
-    backgroundSize: "conver"
+    backgroundSize: "cover"
   } as React.CSSProperties,
   bodyContent: {
-    height: 500,
-    backgroundColor: "#f2f2f2",
+    height: 550,
     fontSize: 36
   } as React.CSSProperties
 };
@@ -26,8 +25,29 @@ export class Home extends React.Component {
         <div style={styles.parallax} />
 
         <div style={styles.bodyContent}>
-          Scroll Up and Down this page to see the parallax scrolling effect.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget velit rhoncus elit fringilla ultrices vel fringilla arcu. Nunc gravida eros eu volutpat egestas. In hac habitasse platea dictumst. Proin eget sodales urna. Praesent posuere lectus eu libero blandit, a venenatis ipsum facilisis. Pellentesque nec vehicula libero, ac sollicitudin urna. Fusce ultricies molestie velit.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro,
+          nesciunt cum nemo natus a impedit quis quae ex eum atque totam ad
+          dolore, eos autem omnis dignissimos voluptatem hic itaque tempore
+          ullam? Voluptatibus veritatis odio eos rem mollitia illo quo expedita
+          porro aspernatur distinctio debitis, adipisci quia sapiente possimus
+          soluta enim id voluptas exercitationem amet iusto! Eius, distinctio
+          vel! Debitis distinctio, ipsam neque laudantium omnis praesentium
+          facilis voluptas, doloribus saepe quis reiciendis animi sapiente cum
+          nemo ut, eius eveniet maxime expedita deleniti nobis numquam! Beatae,
+          consequatur corporis nulla a ipsum cupiditate non ad veniam earum
+          omnis doloribus laboriosam placeat incidunt nesciunt, perferendis
+          animi fugit mollitia voluptas. Molestias, molestiae harum! Assumenda
+          commodi error inventore obcaecati, recusandae repellendus voluptatem
+          earum itaque vitae praesentium quidem tempore ad eligendi minima quos
+          molestiae totam aperiam accusantium est quod quo officia. Minima enim,
+          maiores nemo beatae unde dolores, ea molestias repellendus eum impedit
+          iure quisquam esse. Reprehenderit omnis quas sapiente, iste commodi
+          magni eaque consequuntur incidunt, unde impedit natus dolorum dolore
+          quaerat, dolor obcaecati. Quasi iusto corrupti sunt, adipisci
+          molestias iste dicta. Eius expedita ea magni est quibusdam. Numquam,
+          non sequi aperiam ipsum, saepe, quibusdam atque quidem possimus
+          recusandae fugiat obcaecati eveniet itaque neque natus dolorem!
+          Inventore, repudiandae.
         </div>
         <div style={styles.parallax} />
       </div>
