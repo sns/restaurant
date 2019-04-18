cc0e814f442d12eda44245ec6f386ea19d3981fb
Sina Salehi <sinasalehi@Sinas-MacBook-Pro.local>
2019-04-17 21:43:23 -0400

    created the hok branch

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,13 +4,8 @@ import * as ReactDom from "react-dom";
 import Sidebar from "@src/Common/Components/Sidebar";
 import { Provider } from "react-redux";
 import { configureStore } from "./Common/Store";
-// import { createStore } from "redux";
-// import { rootReducer } from "@src/Common/Store/RootReducer";
-// import { devToolsEnhancer } from "redux-devtools-extension";
-
 dotenv.config();
 
-// const store = createStore(rootReducer, devToolsEnhancer({}));
 const store = configureStore();
 ReactDom.render(
   <Provider store={store}>
