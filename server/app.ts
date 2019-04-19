e95c3af8695e11120e24faf39e003801ed874130
Sina Salehi <sinasalehi@Sinas-MacBook-Pro.local>
2019-04-19 02:25:49 -0400

    serve the static file with express

--- server/app.ts
+++ server/app.ts
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "production") {
 
 // Build Mode
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../client/public", "index.html"));
 });
 
 app.listen(port, () => {
