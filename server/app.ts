ff7c1c4e7481b30f4cf31c22498b75be89860374
Sina Salehi <sinasalehi@Sinas-MacBook-Pro.local>
2019-04-19 02:05:58 -0400

    serve the static file with express

--- server/app.ts
+++ server/app.ts
@@ -7,6 +7,16 @@ const port = process.env.PORT || 3456;
 // Static file declaration
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
+app.get("/fetchMenuItems", (req, res) => {
+  const list = [
+    { menuItemKey: 1, name: "Barg", price: 17 },
+    { menuItemKey: 2, name: "Lamb", price: 18 },
+    { menuItemKey: 3, name: "Chicken", price: 16 },
+    { menuItemKey: 4, name: "Salmon", price: 22 }
+  ];
+  res.json(list);
+});
+
 // Production Mode
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
@@ -18,16 +28,6 @@ if (process.env.NODE_ENV === "production") {
 // Build Mode
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-});
-
-app.get("/fetchMenuItems", (req, res) => {
-  const list = [
-    { menuItemKey: 1, name: "Barg", price: 17 },
-    { menuItemKey: 2, name: "Lamb", price: 18 },
-    { menuItemKey: 3, name: "Chicken", price: 16 },
-    { menuItemKey: 4, name: "Salmon", price: 22 }
-  ];
-  res.json(list);
 });
 
 app.listen(port, () => {
