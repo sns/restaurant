70972f985c66790368d31a7b0869a49eb7f10112
Sina Salehi <sinasalehi@L321-SinaSalehi.local>
2019-09-11 23:59:38 -0400

    used cards on the home page and fixed the responsiveness

--- client/src/common/components/home.tsx
+++ client/src/common/components/home.tsx
@@ -1,4 +1,7 @@
 import * as React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
 
 const styles = {
     showcaseStyle: {
@@ -20,17 +23,8 @@ const styles = {
         boxShadow: "inset 0 0 0 1000px rgba(0,0,0,0.3)",
     } as React.CSSProperties,
     section1: {
-        textAlign: "center",
         padding: 30,
         background: "rgba(255, 246, 242, 0.94)",
-        display: "grid",
-        gridTemplateAreas: `'header'
-                            '.'  
-                            'intro'
-                            '.'
-                             'awards'`,
-        gridTemplateRows: "70px 10px 2fr 50px 1fr",
-        gridTemplateColumns: "auto",
     } as React.CSSProperties,
     intro: {
         gridArea: "intro",
@@ -40,29 +34,23 @@ const styles = {
         padding: 50,
         background: "rgba(255, 246, 242, 0.94)",
         display: "grid",
-        gridTemplateAreas: "'catering . about'",
-        gridTemplateColumns: "1fr 50px 1fr",
-    } as React.CSSProperties,
-    catering: {
-        gridArea: "catering",
-    } as React.CSSProperties,
-    about: {
-        gridArea: "about",
+        gridTemplateColumns: "repeat(auto-fit, minmax(240px, 1fr))",
+        gridGap: 30,
     } as React.CSSProperties,
     showcaseHeaderStyle: {
         gridArea: "header",
         alignSelf: "center",
     } as React.CSSProperties,
     awards: {
-        gridArea: "awards",
         fontSize: "17px",
         lineHeight: "1.5",
         fontStyle: "italic",
+        textAlign: "center",
+        paddingTop: 20,
     } as React.CSSProperties,
     primaryTextPragrph: {
         fontSize: "17px",
         lineHeight: "1.5",
-        textAlign: "justify",
     } as React.CSSProperties,
 };
 export class Home extends React.Component {
@@ -71,65 +59,68 @@ export class Home extends React.Component {
             <div>
                 <div style={styles.showcaseStyle} />
                 <div style={styles.section1}>
-                    <h1 style={styles.showcaseHeaderStyle}>
-                        Welcome to House Of Kabob
-                    </h1>
-                    <div style={styles.intro}>
-                        <p style={styles.primaryTextPragrph}>
-                            Based on Ancient Persia’s thousands of year-old
-                            recipies, House of Kabob offers an exquisite taste
-                            of middle-eastern dining. Whether it’s our famous
-                            ground beef Koobideh kabob, succulent lamb
-                            tenderloin kabob, or the delicately spiced broiled
-                            salmon, our food will definitely leave a satisfied
-                            palate. Whether it’s a romantic night out or an
-                            office party, the House of Kabob is perfect for
-                            every occasion. All of our mouth-watering foods are
-                            available for catering, carry-out, or fine
-                            in-restaurant dining.
-                        </p>
-                    </div>
-                    <div style={styles.awards}>
-                        <div>
-                            City Papaer — Winner of The Best Middle Eastern
-                            Restaurant Best of
-                        </div>
-                        <div>
-                            Baltimore — Voted Best Kabob Baltimore Magazine —
-                            Best Kabob of Baltimore
-                        </div>
-                    </div>
+                    <Card>
+                        <CardContent style={styles.intro}>
+                            <Typography style={styles.primaryTextPragrph}>
+                                Based on Ancient Persia’s thousands of year-old
+                                recipies, House of Kabob offers an exquisite
+                                taste of middle-eastern dining. Whether it’s our
+                                famous ground beef Koobideh kabob, succulent
+                                lamb tenderloin kabob, or the delicately spiced
+                                broiled salmon, our food will definitely leave a
+                                satisfied palate. Whether it’s a romantic night
+                                out or an office party, the House of Kabob is
+                                perfect for every occasion. All of our
+                                mouth-watering foods are available for catering,
+                                carry-out, or fine in-restaurant dining.
+                            </Typography>
+                            <Typography style={styles.awards}>
+                                City Papaer — Winner of The Best Middle Eastern
+                                Restaurant Best of
+                            </Typography>
+                            <Typography style={styles.awards}>
+                                Baltimore — Voted Best Kabob Baltimore Magazine
+                                — Best Kabob of Baltimore
+                            </Typography>
+                        </CardContent>
+                    </Card>
                 </div>
                 <div style={styles.showcaseStyle2} />
                 <div style={styles.section2}>
-                    <div style={styles.catering}>
-                        <p style={styles.primaryTextPragrph}>
-                            Having a party and can’t come to the House of Kabob
-                            for dining in, yet still want our scrumptious food?
-                            No problem! We offer all of our food on the menu for
-                            catering. The finest, freshest Persian food made
-                            with special care that you would expect to have here
-                            can be had at your own home. Quality first, as with
-                            everything we do, can be guaranteed with our
-                            exceptional catering services. Delivery and/or
-                            chafing dishes are available upon request for an
-                            additional surcharge. Whether it is 20 orders or
-                            200, or even more, it is no problem!
-                        </p>
-                    </div>
-                    <div style={styles.about}>
-                        <p style={styles.primaryTextPragrph}>
-                            Haven’t tried Persian cuisine before and don’t know
-                            what to order? No problem! Come to our buffet where
-                            we offer a wide variety of some of our best dishes.
-                            Everything is made fresh daily: soup, salads,
-                            kabobs, stews, our homemade Persian flat bread, and
-                            more. Our buffet is available Tuesday through
-                            Thursday from 11:30 am to 3:30 pm, and all day
-                            Fridays, Saturdays, and Sundays. Come to House of
-                            Kabob for the best in Persian dining!
-                        </p>
-                    </div>
+                    <Card>
+                        <CardContent>
+                            <Typography style={styles.primaryTextPragrph}>
+                                Having a party and can’t come to the House of
+                                Kabob for dining in, yet still want our
+                                scrumptious food? No problem! We offer all of
+                                our food on the menu for catering. The finest,
+                                freshest Persian food made with special care
+                                that you would expect to have here can be had at
+                                your own home. Quality first, as with everything
+                                we do, can be guaranteed with our exceptional
+                                catering services. Delivery and/or chafing
+                                dishes are available upon request for an
+                                additional surcharge. Whether it is 20 orders or
+                                200, or even more, it is no problem!
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardContent>
+                            <Typography style={styles.primaryTextPragrph}>
+                                Haven’t tried Persian cuisine before and don’t
+                                know what to order? No problem! Come to our
+                                buffet where we offer a wide variety of some of
+                                our best dishes. Everything is made fresh daily:
+                                soup, salads, kabobs, stews, our homemade
+                                Persian flat bread, and more. Our buffet is
+                                available Tuesday through Thursday from 11:30 am
+                                to 3:30 pm, and all day Fridays, Saturdays, and
+                                Sundays. Come to House of Kabob for the best in
+                                Persian dining!
+                            </Typography>
+                        </CardContent>
+                    </Card>
                 </div>
             </div>
         );
